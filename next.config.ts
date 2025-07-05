545169a2e5c01d41f3531d76ec11b7e2dd615f92
Đỗ Thái Sơn <doson7714@gmail.com>
2025-07-06 02:18:20 +0700

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-  output: 'export',
-  trailingSlash: true,
-=======
->>>>>>> 3696b98 (initial scaffold)
   typescript: {
     ignoreBuildErrors: true,
   },
